87891338c33b8f3aa205c8517f1b863a83054d3c
copenluu <ocollinscope@gmail.com>
2022-09-23 16:52:53 +0100

    <main>: added raycast doors

---
+++ HoH/Assets/Scripts/MyDoorController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyDoorController : MonoBehaviour
+{
+    private Animator doorAnim;
+
+    private bool doorOpen = false;
+
+    private void Awake()
+    {
+        doorAnim = gameObject.GetComponent<Animator>();
+    }
+
+    public void PlayAnimation()
+    {
+        if (!doorOpen)
+        {
+            doorAnim.Play("doorOpens", 0, 0.0f);
+            doorOpen = true;
+        }
+        else
+        {
+            doorAnim.Play("doorClose", 0, 0.0f);
+            doorOpen = false;
+        }
+    }
+
+}
--- HoH/Assets/Scripts/RaycastDoor.cs
+++ HoH/Assets/Scripts/RaycastDoor.cs
@@ -4,163 +4,42 @@ using UnityEngine;
 
 public class RaycastDoor : MonoBehaviour
 {
+    [Header("Raycast length")]
+    [SerializeField] private int rayLength = 5;
+    
+    [Header("Layer Mask")]
+    [SerializeField] private LayerMask layerMaskInteract;
 
+    [Header("Exclusions")]
+    [SerializeField] private string excludeLayerName = null;
 
-    [Header("Raycast Distance")]
-    [SerializeField] private int raycastDist;
+    private MyDoorController raycastedObj;
 
-    [Header("Lock settings")]
-    [SerializeField] private bool locked;
-    [SerializeField] private bool unlocked;
+    [Header("Interact Button")]
+    [SerializeField] private KeyCode openDoorKey = KeyCode.E;
 
-    [Header("Animator")]
-    [SerializeField] private Animator door;
-
-    [Header("Game Objects")]
-    [SerializeField] private GameObject lockOB;
-    [SerializeField] private GameObject keyOB;
-    [SerializeField] private GameObject openText;
-    [SerializeField] private GameObject closeText;
-    [SerializeField] private GameObject lockedText;
-
-    [Header("Audio")]
-    [SerializeField] private AudioSource openSound;
-    [SerializeField] private AudioSource closeSound;
-    [SerializeField] private AudioSource lockedSound;
-    [SerializeField] private AudioSource unlockedSound;
-
-    private bool inReach;
-    private bool doorisOpen;
-    private bool doorisClosed;
-    private bool isAnimating = false;
-
-
-
-    [SerializeField] private Vector3 collision = Vector3.zero;
-
-
-    private void Start()
-    {
-        inReach = false;
-        doorisClosed = true;
-        doorisOpen = false;
-        closeText.SetActive(false);
-        openText.SetActive(false);
-
-    }
+    private const string interactableTag = "interactiveObject";
 
     private void Update()
     {
-        var ray = new Ray(this.transform.position, this.transform.forward);
         RaycastHit hit;
-        if (Physics.Raycast(ray, out hit, raycastDist))
-        {
-            collision = hit.point;
+        Vector3 fwd = transform.TransformDirection(Vector3.forward);
 
-            if (hit.transform.gameObject.CompareTag("DoorHinge"))
+        int mask = 1 << LayerMask.NameToLayer(excludeLayerName) | layerMaskInteract.value;
+
+        if (Physics.Raycast(transform.position, fwd, out hit, rayLength, mask))
+        {
+            if (hit.collider.CompareTag(interactableTag))
             {
-                Debug.Log("Door Hinge");
+                raycastedObj = hit.collider.gameObject.GetComponent<MyDoorController>();
+                Debug.Log("Hitting Door");
             }
 
-            if (hit.transform.gameObject.CompareTag("DoorHinge") && doorisClosed)
+            if (Input.GetKeyDown(openDoorKey))
             {
-                inReach = true;
-                openText.SetActive(true);
-            }
-            else if (hit.transform.gameObject.CompareTag("DoorHinge") && doorisOpen)
-            {
-                inReach = true;
-                closeText.SetActive(true);
-            }
-            else if (!hit.transform.gameObject.CompareTag("DoorHinge"))
-            {
-                inReach = false;
-                openText.SetActive(false);
-                closeText.SetActive(false);
-                lockedText.SetActive(false);
+                raycastedObj.PlayAnimation();
             }
         }
-
-
-
-            if (lockOB.activeInHierarchy)
-        {
-            locked = true;
-            unlocked = false;
-        }
-        else
-        {
-            locked = false;
-            unlocked = true;
-        }
-
-        if (inReach && keyOB.activeInHierarchy && Input.GetButtonDown("Interact"))
-        {
-            unlockedSound.Play();
-            locked = false;
-            keyOB.SetActive(false);
-            StartCoroutine(unlockDoor());
-        }
-
-        if (inReach && doorisClosed && unlocked && Input.GetButtonDown("Interact") && isAnimating == false)
-        {
-            door.SetBool("Open", true);
-            door.SetBool("Closed", false);
-            openText.SetActive(false);
-            openSound.Play();
-            doorisOpen = true;
-            doorisClosed = false;
-
-        }
-
-        else if (inReach && doorisOpen && unlocked && Input.GetButtonDown("Interact") && isAnimating == false)
-        {
-            door.SetBool("Open", false);
-            door.SetBool("Closed", true);
-            closeText.SetActive(false);
-            closeSound.Play();
-            doorisOpen = false;
-            doorisClosed = true;
-        }
-
-
-        if (inReach && locked && Input.GetButtonDown("Interact"))
-        {
-            openText.SetActive(false);
-            lockedText.SetActive(true);
-            lockedSound.Play();
-        }
-
-
-
-
-    }
-
-    IEnumerator unlockDoor()
-    {
-        yield return new WaitForSeconds(0.05f);
-        {
-            unlocked = true;
-            lockOB.SetActive(false);
-        }
-    }
-
-    private void DisableInteract()
-    {
-        isAnimating = true;
-    }
-
-    private void EnableInteract()
-    {
-        isAnimating = false;
-    }
-
-
-    private void OnDrawGizmos()
-    {
-        Update();
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(collision, 0.2f);
     }
 
 
