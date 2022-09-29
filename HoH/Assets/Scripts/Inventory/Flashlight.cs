e40872bbcd7f11de1802e9b467f7d6963ee40c7b
copenluu <ocollinscope@gmail.com>
2022-09-29 23:36:45 +0100

    <main>: added prefabs to build

--- HoH/Assets/Scripts/Inventory/Flashlight.cs
+++ HoH/Assets/Scripts/Inventory/Flashlight.cs
@@ -2,8 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Flashlight : MonoBehaviour
 {
+
+
     [Header("Flashlight")]
     [SerializeField] private GameObject flashlight;
 
@@ -11,31 +14,28 @@ public class Flashlight : MonoBehaviour
     [SerializeField] private AudioSource turnOn;
     [SerializeField] private AudioSource turnOff;
 
-    [Header("On and off")]
+    [Header("On")]
     [SerializeField] private bool flashlightOn;
-    [SerializeField] private bool flashlightOff;
 
 
     private void Start()
     {
-        flashlightOff = true;
+        flashlightOn = false;
         flashlight.SetActive(false);
     }
 
     private void Update()
     {
-        if (flashlightOff && Input.GetButtonDown("F"))
+        if (!flashlightOn && Input.GetButtonDown("F"))
         {
             flashlight.SetActive(true);
             turnOn.Play();
-            flashlightOff = false;
             flashlightOn = true;
         }
         else if (flashlightOn && Input.GetButtonDown("F"))
         {
             flashlight.SetActive(false);
             turnOff.Play();
-            flashlightOff = true;
             flashlightOn = false;
         }
     }
