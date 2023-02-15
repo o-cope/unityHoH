9fd75f661b103cb8c7acc99f79b33e2a0145d3ec
copenluu <ocollinscope@gmail.com>
2023-02-15 16:52:05 +0000

    <main>: making main menu

--- HoH/Assets/Scripts/Interactables/Misc/FlashlightPickUpController.cs
+++ HoH/Assets/Scripts/Interactables/Misc/FlashlightPickUpController.cs
@@ -1,15 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class FlashlightPickUpController : Interactable
 {
     #region Parametres
     [Header("Player Object")]
     [SerializeField] private GameObject player;
+
+    [Header("Flashlight")]
+    [SerializeField] private GameObject flashlightObj;
+
+    [Header("Audio")]
+    [SerializeField] private AudioSource pickUpFlashlight;
     
-    
-    private Flashlight flashlight;
+    [Header("Wall")]
+    [SerializeField] private GameObject[] walls = new GameObject[2];
+
+    [Header("Canvas")]
+    [SerializeField] private Canvas canvasFlashlight;
+    [SerializeField] private Canvas canvasEnableText; //Assigns variables
+
+    private Flashlight flashlight; //References script
 
 
     #endregion
@@ -21,15 +34,26 @@ public class FlashlightPickUpController : Interactable
 
     protected override void Interact()
     {
-        PickUp();
+        PickUp(); //When player interacts
     }
 
     #region Functions
 
     private void PickUp()
     {
-        flashlight.canUseFlashlight = true;
+        pickUpFlashlight.Play(); //Plays Audio
+        flashlightObj.SetActive(false); //Disables Flashlight Object in game
+        flashlight.canUseFlashlight = true; //Allows player to use flashlight
+
+        canvasFlashlight.enabled = false; //Disables text
+        canvasEnableText.gameObject.SetActive(true); //Enables other text
+
+        Destroy(GetComponent<BoxCollider>()); //Stops the player from being able to interact with nonexistent flashlight
+        Destroy(walls[0]);
+        Destroy(walls[1]);   //Destroys obstructions allowing player to play
     }
+
+
 
     #endregion
 }
